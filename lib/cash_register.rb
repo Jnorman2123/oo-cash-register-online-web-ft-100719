f5edd86d2b90b9e9f5aa66c709941a69d9afd696
Jonathan Norman <jnorman2123@gmail.com>
2019-10-17 23:02:30 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -5,7 +5,23 @@ class CashRegister
     @total = 0
   end
 
-  def discount
+  def total 
+
+  end 
+
+  def add_item 
+
+  end 
+
+  def apply_ discount
 
   end
+
+  def items 
+
+  end 
+
+  def void_last_transaction
+
+  end 
 end
